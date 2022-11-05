6bd6f5a06ed6664ba587852f4a02f1cfbfcf6c7b
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2022-11-05 21:32:41 +0530

    made some file system changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,1 +1,9 @@
 import Navbar from "./Navbar"
+
+const Home = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default Home
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,11 @@ import styled from "styled-components"
 
 
 const Container = styled.div`
-    
+    height: 20px;
+    width: 100%;
+    z-index: 1;
+    border: none;
+    background-color: #001a33;
 `
 
 const Navbar = () => {
